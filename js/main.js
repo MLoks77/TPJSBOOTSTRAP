3f6afbe7a65d9da35fc761ab6b271bac4cf2e4a7
MLoks77 <maximederenes@gmail.com>
2025-05-15 17:57:46 +0200

    ajout des données, attention js nom

--- js/main.js
+++ js/main.js
@@ -1,10 +1,83 @@
-function rechercher(){
+document.getElementById('p-chambres').addEventListener('click', addchambre);
 
-const pays = document.getElementById('');
-const nom = document.getElementById('');
+// utiliser les bons noms, là c mauvais
+
+function addchambre() {
+    const chambresInput = document.getElementById('chambres');
+    chambresInput.value = parseInt(chambresInput.value, chambres) + 1;
+}
+
+function rmvchambre() {
+    const chambresInput = document.getElementById('chambres');
+    if (parseInt(chambresInput.value, 10) === 0) {
+        alert("Vous ne pouvez pas avoir moins de 0 chambres");
+    } else {
+        chambresInput.value = parseInt(chambresInput.value, 10) - 1;
+    }
+}
+
+function addchambre() {
+    const chambresInput = document.getElementById('chambres');
+    chambresInput.value = parseInt(chambresInput.value, chambres) + 1;
+}
+
+function rmvchambre() {
+    const chambresInput = document.getElementById('chambres');
+    if (parseInt(chambresInput.value, 10) === 0) {
+        alert("Vous ne pouvez pas avoir moins de 0 chambres");
+    } else {
+        chambresInput.value = parseInt(chambresInput.value, 10) - 1;
+    }
+}
+
+function rechercher() {
+
+const pays = document.getElementById('pays');
+const adultes = document.getElementById('adultes');
+const enfants = document.getElementById('enfants');
+const chambres = document.getElementById('chambres');
+const arrive = document.getElementById('arrive');
+const depart = document.getElementById('depart');
+
+const chambresplus = document.getElementById('p-chambres');
+const chambresmoins = document.getElementById('m-chambres');
+const enfantplus = document.getElementById('p-enfants');
+const enfantsmoins = document.getElementById('m-enfants');
+
+const travail = document.getElementById('travail');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let msgtravail;
+if(document.getElementById("travail").checked){
+    msgtravail = "Vous voyagez pour le travail";
+} else {
+    msgtravail = "Vous ne voyagez pas pour le travail";
+}
+
+document.getElementById('msgtravail').innerText = msgtravail;
+
 }
 
 function annuler() {
-    document.getElementById('salairebrut').value = 0;
-    document.getElementById('travail').checked = false;
+document.getElementById('msgtravail').innerText = "";
 }
+
+addchambre() 
+
+rmvchambre() 
+
+addenfant() 
+
+rmvenfant()
