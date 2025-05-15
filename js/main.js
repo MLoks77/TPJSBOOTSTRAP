fea29154ff3579c2b44bd3037428291ce178cd3e
MLoks77 <maximederenes@gmail.com>
2025-05-15 12:10:23 +0200

    ajout bouton + - , à corriger

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function rechercher(){
+
+const pays = document.getElementById('');
+const nom = document.getElementById('');
+}
+
+function annuler() {
+    document.getElementById('salairebrut').value = 0;
+    document.getElementById('travail').checked = false;
+}
